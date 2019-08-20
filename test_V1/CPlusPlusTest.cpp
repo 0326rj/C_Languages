68ef1979bc16a8612ac04f0942f858af71c896fd
Jaebum Kim <0326rj@gmail.com>
2019-08-21 00:22:13 +0900

    c

--- test_V1/CPlusPlusTest.cpp
+++ test_V1/CPlusPlusTest.cpp
@@ -1,41 +1,20 @@
-// test_V1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <ctime>
 #include <cstdlib>
-#include <string>
+#include <ctime>
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	string str;
+	double x;
 
-	cout << "–¼‘O‚ð“ü—Í";
-	getline(cin, str);
+	cout << "double..";
+	cin >> x;
 
-	cout << str << "‚³‚ñ";
-
-	// if
-	int n;
-	cout << "integer input";
-	cin >> n;
-
-	if (n > 0)
+	if ( double n = fmod( x, 10 ) )
 	{
-
 	}
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
