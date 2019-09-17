cbb7d6f3e0f4ebe7310e3a5e7d7a867e0b323fc0
Jaebum Kim <0326rj@gmail.com>
2019-09-17 22:13:41 +0900

    test

--- test_V1/CPlusPlusTest.cpp
+++ test_V1/CPlusPlusTest.cpp
@@ -1,25 +1,41 @@
-
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
+#include <climits>
+#include <typeinfo>
+
+#define INT_MIN -32768
 
 using namespace std;
 
+
+double power(double x, double n)
+{
+	double tmp = 1.0;
+	for ( int i = 1; i <= n; i++ )
+	{
+		tmp *= x;
+	}
+	return tmp;
+}
+
+double power2( double x, double n )
+{
+	double tmp = 1.0;
+	while ( n-- > 0 )
+	{
+		tmp *= x;
+	}
+	return tmp;
+}
+
 int main()
 {
-	int x;
-
-	do
-	{
-		cin >> x;
-
-	} while ( x <= 0 );
-
-	while ( x >= 0 )
-	{
-		cout << x << "\n";
-		x--;
-	}
+	double x, n;
+	cin >> x; cin >> n;
+	
+	cout << power2( x, n ) << '\n';
 }
